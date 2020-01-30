a7cab4387ba3bef25802819ca47170ad3a4209a0
Ken Kousen <ken.kousen@kousenit.com>
2020-01-30 12:29:08 -0500

    Add characters to varchar for last name

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE officers (
   id         INT         NOT NULL AUTO_INCREMENT,
   rank       VARCHAR(20) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
-  last_name  VARCHAR(20) NOT NULL,
+  last_name  VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
