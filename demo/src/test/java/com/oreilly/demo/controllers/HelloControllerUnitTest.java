dd64e391c0082a88d197e32b9f206c2672fa921f
Ken Kousen <ken.kousen@kousenit.com>
2020-08-11 10:07:41 -0400

    Add test using mock model

--- demo/src/test/java/com/oreilly/demo/controllers/HelloControllerUnitTest.java
+++ demo/src/test/java/com/oreilly/demo/controllers/HelloControllerUnitTest.java
@@ -6,6 +6,10 @@ import org.springframework.validation.support.BindingAwareModelMap;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class HelloControllerUnitTest {
 
@@ -18,5 +22,15 @@ public class HelloControllerUnitTest {
                 () -> assertEquals("World", model.asMap().get("user")),
                 () -> assertEquals("hello", result)
         );
+    }
+
+    @Test  // "true" unit test
+    void sayHelloWithStub() {
+        HelloController controller = new HelloController();
+        Model mockModel = mock(Model.class);
+        // set expectations on mock
+        when(mockModel.addAttribute(anyString(), any())).thenReturn(mockModel);
+        String result = controller.sayHello("Dolly", mockModel);
+        assertEquals("hello", result);
     }
 }
