c230c60bcb38afae133b308d8e806868bf5554b5
Ken Kousen <ken.kousen@kousenit.com>
2020-08-12 07:58:28 -0400

    Add before aspect for logging

---
+++ demo/src/main/java/com/oreilly/demo/aspects/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.oreilly.demo.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.oreilly.demo.*.*.*(..))")
+    public void logMethodCalls(JoinPoint joinPoint) {
+        logger.info("Entering  " + joinPoint.getSignature());
+        logger.info("with args " + Arrays.toString(joinPoint.getArgs()));
+    }
+}
